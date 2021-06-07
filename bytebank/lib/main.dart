49caf69c65f2777d8a037ed224d024042df29cd5
Pedro Gabriel <pedrogabriel12423@gmail.com>
2021-06-07 11:11:57 -0300

    Adicionando repositórios iniciais

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      Column(
+        children: const <Widget>[
+          Text('Deliver features faster', textDirection: TextDirection.ltr,),
+          Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+         
+        ],
+      )
+  );
+}
