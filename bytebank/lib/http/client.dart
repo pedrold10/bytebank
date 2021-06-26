767a9291843bd2e374650f6e032d3c7b1008f53e
Pedro Gabriel <pedrogabriel12423@gmail.com>
2021-06-26 16:20:05 -0300

    Função de criar transferências

--- bytebank/lib/http/client.dart
+++ bytebank/lib/http/client.dart
@@ -24,23 +24,55 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
+final Client client = InterceptedClient.build(
+  interceptors: [
+    LoggingInterceptor(),
+  ],
+);
+final Uri baseUrl = Uri.http('192.168.56.1:8080', 'transactions');
+
 Future<List<Transaction>> findAll() async {
-  Client client = InterceptedClient.build(interceptors: [LoggingInterceptor()]);
-  final Response response =
-      await client.get(Uri.http('192.168.0.122:8080', 'transactions')).timeout(Duration(seconds: 5));
+  final Response response = await client
+      .get(baseUrl)
+      .timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = [];
   for (Map<String, dynamic> transactionJson in decodedJson) {
     final Map<String, dynamic> contactJson = transactionJson['contact'];
     final Transaction transaction = Transaction(
-        transactionJson['value'],
-        Contact(
-          0,
-          contactJson['name'],
-          contactJson['contact']['accountNumber'],
-        ),
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['contact']['accountNumber'],
+      ),
     );
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  };
+  final String transactionJson = jsonEncode(transactionMap);
+  final Response response = await client.post(baseUrl, headers: {
+    'Content-type': 'application/json',
+    'password': '1000'
+  }, body: transactionJson);
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['contact']['accountNumber'],
+    ),
+  );
 }
--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'http/client.dart';
+import 'modelos/contact.dart';
+import 'modelos/transaction.dart';
 import 'src/dashboard.dart';
 void main() {
   runApp(MyApp());
-  findAll().then((transactions)=>print('new transactions ${transactions}'));
+  save(Transaction(200.0, Contact(0, 'Carlos', 2000))).then((transaction)=>print(transaction));
+  //findAll().then((transactions)=>print('new transactions ${transactions}'));
 }
 class MyApp extends StatelessWidget {
   @override
